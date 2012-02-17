fc5ffef0eceb147a800d9e753e995fde573f5bf5
taoqian <simonvy@gmail.com>
2012-02-17 22:58:10 +0800

    Initial Commit

---
+++ iTunesShuffle/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iTunesShuffle
+//
+//  Created by Qian Tao on 2/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ iTunesShuffle/AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  iTunesShuffle
+//
+//  Created by Qian Tao on 2/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
